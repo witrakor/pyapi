7cf12c08b005b1a345b1b13aa57007ba53a522af
witrakor <witrakor@hotmail.com>
2025-03-12 16:07:10 +0700

    Second commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from .routers import book, user, file
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "https://stackpython.co"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(book.router)
+app.include_router(user.router)
+app.include_router(file.router)
+
+@app.get("/")
+async def root():
+    
+    return {"message": "Hello World"}
